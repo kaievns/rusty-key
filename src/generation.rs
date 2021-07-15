dd5c1bca9f29dce7fc02054cdfac40532e7bdf58
Nikolay Nemshilov <nemshilov@gmail.com>
2021-07-16 09:42:00 +1000

    Exposing the whole calculation results on generetions

--- src/generation.rs
+++ src/generation.rs
@@ -8,11 +8,19 @@ use crate::keyboard::*;
 use crate::calculator::*;
 use crate::population::*;
 use crate::selection::*;
+use crate::summary::*;
 use crate::profiler;
 
 pub struct Generation {
   pub number: usize,
   pub population: Population
+}
+
+#[derive(Debug,PartialEq)]
+pub struct Result {
+  summary: Summary,
+  fitness: f64,
+  deviation: f64
 }
 
 impl Generation {
@@ -31,21 +39,19 @@ impl Generation {
     Generation::new(self.number + 1, &layout)
   }
 
-  pub fn successor(self: &Self) -> Layout {
+  pub fn successor(self: &Self) -> &'static Layout {
     static SUCCESSOR: OnceCell<Layout> = OnceCell::new();
     SUCCESSOR.get_or_init(|| {
-      let selection = self.create_selection();
+      let selection = self.fetch_selection();
       let score = selection.lucky_draw();
       let index = selection.scores.iter()
         .position(|s| s == score).unwrap();
 
-      let layout = self.population.members.get(index).unwrap();
-
-      (*layout).clone()
-    }).clone()
+      (*self.population.members.get(index).unwrap()).clone()
+    })
   }
 
-  pub fn best(&self) -> Layout {
+  pub fn best(&self) -> &'static Layout {
     static BEST: OnceCell<Layout> = OnceCell::new();
     BEST.get_or_init(|| {
       let mut ratings: Vec<(usize, f64)> = self.population.members.iter().enumerate()
@@ -54,37 +60,52 @@ impl Generation {
       ratings.sort_by(|a, b| b.1.partial_cmp(&a.1).unwrap_or(Less));
       let best_rating = *ratings.first().unwrap();
 
-      let layout = self.population.members.get(best_rating.0).unwrap();
-
-      (*layout).clone()
-    }).clone()
+      (*self.population.members.get(best_rating.0).unwrap()).clone()
+    })
   }
 
   pub fn rating_for(&self, layout: &Layout) -> f64 {
-    let index = self.population.members.iter().position(|l| *l == *layout).unwrap();
-    let score = self.create_selection().scores.get(index).unwrap();
+    let result = self.result_for(layout);
 
-    score.performance * score.fitness
+    result.summary.score() * result.fitness
   }
 
-  fn create_selection(&self) -> &'static Selection {
+  pub fn result_for(&self, layout: &Layout) -> &'static Result {
+    let index = self.population.members.iter().position(|l| *l == *layout).unwrap();
+    self.calculate_results().get(index).unwrap()
+  }
+
+  fn fetch_selection(&self) -> &'static Selection {
     static SELECTION: OnceCell<Selection> = OnceCell::new();
     SELECTION.get_or_init(|| {
-      let scores = self.population.members.par_iter()
-          .map(|layout| self.rate_layout(layout))
+      let scores: Vec<Score> = self.calculate_results().iter()
+          .map(|result| Score {
+            deviation: result.deviation,
+            performance: result.summary.score(),
+            fitness: result.fitness
+          })
           .collect();
         Selection { scores }
     })
   }
 
-  fn rate_layout(self: &Self, layout: &Layout) -> Score {
+  fn calculate_results(&self) -> &'static Vec<Result> {
+    static RESULTS: OnceCell<Vec<Result>> = OnceCell::new();
+    RESULTS.get_or_init(|| {
+      self.population.members.par_iter()
+        .map(|layout| self.rate_layout(layout))
+        .collect()
+    })
+  }
+
+  fn rate_layout(self: &Self, layout: &Layout) -> Result {
     let deviation = self.population.deviation_for(layout);
     let keyboard = Keyboard::from(&layout, &CONFIG.geometry);
     let calculator = Calculator::from(&keyboard);
-    let performance = calculator.run(&CONFIG.data).score();
+    let summary = calculator.run(&CONFIG.data);
     let fitness = profiler::calculate_fitness(&keyboard);
 
-    Score { deviation, performance, fitness }
+    Result { deviation, summary, fitness }
   }
 }
 
@@ -138,5 +159,16 @@ mod test {
     assert_eq!(generation.rating_for(layout1), generation.rating_for(layout1));
     assert_eq!(generation.rating_for(layout1), generation.rating_for(layout1));
     assert_ne!(generation.rating_for(layout1), generation.rating_for(layout2));
+  }
+
+  #[test]
+  fn test_result_for() {
+    let generation = Generation::zero();
+    let layout1 = &generation.population.members[0];
+    let layout2 = &generation.population.members[6].clone();
+
+    assert_eq!(generation.result_for(layout1), generation.result_for(layout1));
+    assert_eq!(generation.result_for(layout1), generation.result_for(layout1));
+    assert_ne!(generation.result_for(layout1), generation.result_for(layout2));
   }
 }
