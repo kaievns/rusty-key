bada9df2b79489d55d57d8f9a4b3f89afcec9dd4
Nikolay Nemshilov <nemshilov@gmail.com>
2021-08-06 16:22:22 +1000

    Shoveling results around to render a table

--- src/ui.rs
+++ src/ui.rs
@@ -3,62 +3,90 @@ use termion::raw::IntoRawMode;
 use termion::event::Key;
 use tui::Terminal;
 use tui::backend::TermionBackend;
-use tui::widgets::{Widget, Block, Borders, Paragraph};
+use tui::text::{Span};
+use tui::style::{Color, Style};
+use tui::widgets::{Block, Borders, Cell, Row, Table};
 use tui::layout::{Layout, Constraint, Direction};
 
 use crate::events::{self,Event};
-use crate::generation::Outcome;
+use crate::model::ViewModel;
 
 pub fn render() -> Result<(), Box<dyn std::error::Error>> {
   let stdout = io::stdout().into_raw_mode()?;
   let backend = TermionBackend::new(stdout);
   let mut terminal = Terminal::new(backend)?;
 
-  let mut outcomes: Vec<Outcome> = vec![];
-  let mut frames: usize = 0;
+  let mut model = ViewModel::new();
 
   terminal.clear();
 
   loop {
-    frames += 1;
-
     terminal.draw(|f| {
+      
+      let main_chunks = Layout::default()
+          .direction(Direction::Horizontal)
+          .margin(1)
+          .constraints(
+            [
+              Constraint::Percentage(70),
+              Constraint::Percentage(30)
+            ].as_ref()
+          )
+          .split(f.size());
+
       let chunks = Layout::default()
           .direction(Direction::Vertical)
-          .margin(1)
+          // .margin(1)
           .constraints(
               [
                   Constraint::Percentage(70),
                   Constraint::Percentage(30)
               ].as_ref()
           )
-          .split(f.size());
-      let block1 = Block::default()
-            .title("Block")
+          .split(main_chunks[0]);
+
+      let table_block = Block::default()
+            .title("Latest results")
             .borders(Borders::ALL);
-      // f.render_widget(block, chunks[0]);
-      let block2 = Block::default()
-            .title("Block 2")
+      // f.render_widget(table_block, chunks[0]);
+
+      let header_cells = ["#", "Layout name", "Score"].iter()
+        .map(|h| Cell::from(*h).style(Style::default().fg(Color::Red)));
+      let header = Row::new(header_cells);
+                // .style(normal_style)
+                // .height(1)
+                // .bottom_margin(1);
+      let top_list = model.top_list();
+      let rows = top_list.iter().map(|line| {
+        let cells = line.iter().map(|c| Cell::from(Span::raw(c)));
+        Row::new(cells) //.bottom_margin(1)
+      });
+      let table = Table::new(rows)
+        .header(header)
+        .block(table_block)
+        .widths(&[
+          Constraint::Length(3),
+          // Constraint::Min(10),
+          Constraint::Length(30),
+          Constraint::Length(6)
+        ]);
+      f.render_widget(table, chunks[0]);
+
+
+      let chart_block = Block::default()
+            .title("Progress")
             .borders(Borders::ALL);
-      f.render_widget(block2, chunks[1]);
+      f.render_widget(chart_block, chunks[1]);
 
-      let resutls = outcomes.iter().map(|outcome| {
-        format!("{} \t{:?}", outcome.best.name(), outcome.best_summary.score())
-      }).collect::<Vec<String>>().join("\n");
-
-      let text = format!(
-        "Frame: {}\nGenerations: {}\nResults:\n{}",
-        frames, outcomes.len(), resutls
-      );
-
-      let par = Paragraph::new(text.clone())
-        .block(block1);
-      f.render_widget(par, chunks[0]);
+      let details_block = Block::default()
+          .title("Best layout")
+          .borders(Borders::ALL);
+      f.render_widget(details_block, main_chunks[1]);
     })?;
 
     match events::inst().next()? {
       Event::Result(outcome) => { // calculation result
-        outcomes.push(outcome);
+        model.record(outcome);
       },
       Event::Input(key) => {  // any key
         if key == Key::Char('q') {
